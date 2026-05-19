115595ab247f43ddfe7eaab2da30f8bc64a919c9
banjie12 <135499718+banjie12@users.noreply.github.com>
2026-05-19 07:09:19 +0100

    Update

--- 0.0.0/scripts/gameControl.js
+++ 0.0.0/scripts/gameControl.js
@@ -11,23 +11,23 @@
 let gameControl = {
     //---Last time and delta time properties---
 
-    //Last time property
+    //Last time property set to currrent time
     lastTime: performance.now(),
 
-    //Delta time property
+    //Delta time property set to null as we need at least one tick for that
     deltaTime: null,
 
     //Tick
     tick: function() {
         //---Calulate last time and delta time---
 
-        //Find current time and store in a constant variable
+        //Find current time and store in a constant variable to use for delta time and last time calculations
         const time = performance.now();
 
-        //Calculate and set delta time using time and last time variables
+        //Calculate and set delta time using time and last time variables to make an fps work
         this.deltaTime = (time - this.lastTime) / 1000;
 
-        //Set last time variable to the time variable
+        //Track the last time to help with delta time calculations
         this.lastTime = time;
     }
 };
